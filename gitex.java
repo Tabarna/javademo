159f90062d2ad4e77783769e577aacff338d34a1
abarna <saina.thiru@gmail.com>
2021-09-21 07:30:51 +0530

    second commit

--- gitex.java
+++ gitex.java
@@ -5,7 +5,7 @@ public class gitex{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("adding comments");
+		System.out.println("adding comments welcome");
 	}
 
 }
